063edc56afe9fd850a365b4aaed3c93e53a1e71b
Avakyan Samson <avakyansamsonsetraki@gmail.com>
2020-06-18 16:48:56 +0300

    added useEffect, deleted componentDidMount()

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Dashboard from './Dashboard'
+import OpenedCard from './OpenedCard'
+
+const App = ({ fetchData, pokemons }) => {
+   const [value, setValue] = useState('')
+   useEffect(() => {
+    fetchData(0, 20)
+  }, [])
+
+  return (
+    <div className="container">
+      <input
+        className="search"
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.value)}
+      />
+      <Dashboard pokemons={pokemons} />
+      <button className="show-more-btn" onClick={() => fetchData(0, 40)}>
+        Show more...
+      </button>
+      <Switch>
+        <Route exact path="/cards/:id" component={OpenedCard} />
+      </Switch>
+    </div>
+  )
+}
+
+export default App
--- src/redux/containers/App.js
+++ src/redux/containers/App.js
@@ -1,29 +1,7 @@
-import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import Dashboard from '../../components/Dashboard'
-import OpenedCard from '../../components/OpenedCard'
+import App from '../../components/App'
 import { pokemonsFetchData } from '../actions'
-
-class App extends Component {
-  componentDidMount() {
-    this.props.fetchData(0, 20)
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <input className="search" />
-        <Dashboard pokemons={this.props.pokemons} />
-        <button className="show-more-btn">Show more...</button>
-        <Switch>
-          <Route exact path="/cards/:id" component={OpenedCard} />
-        </Switch>
-      </div>
-    )
-  }
-}
 
 const mapStateToProps = (state) => {
   return {
